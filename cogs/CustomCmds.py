a4e1880a9bc3a8ccea9161f622ba658a8a5d9018
a3r0 <nitusingh14372@gmail.com>
2023-09-10 15:04:21 +0530

    rules cmd

--- cogs/CustomCmds.py
+++ cogs/CustomCmds.py
@@ -21,8 +21,8 @@ class CustomCmds(commands.Cog):
     @tasks.loop(seconds=3600)
     async def auto_roster(self):
         guild = self.bot.get_guild(int(905665593267609631))
-        FOUNDER = guild.get_role(992648475634315355)
-        OWNER = guild.get_role(992648660166910073)
+        FOUNDER = guild.get_role(992648660166910073)
+        OWNER = guild.get_role(91150133179110404127)
         BOT_DEV = guild.get_role(992651330466361414)
         DESIGNERS = guild.get_role(992651587577196686)
 
@@ -34,7 +34,7 @@ class CustomCmds(commands.Cog):
         embed = discord.Embed(color=rcfg.CLR, timestamp=discord.utils.utcnow())
         embed.set_footer(text="Last updated on")
         embed.title = "GRAPHICS CODE 2.0 ROSTER"
-        embed.add_field(name=f'<:graphicscode:985936754446073958> 𒁍〢__FOUNDERS:__ ', value=f'{foun}' or 'None',
+        embed.add_field(name=f'<:graphicscode:985936754446073958> 𒁍〢__CEO:__ ', value=f'{foun}' or 'None',
                         inline=False)
         embed.add_field(name=f'<:graphicscode:985936754446073958> 𒁍〢__CEO:__ ', value=f'{owne}' or 'None',
                         inline=False)
@@ -167,17 +167,17 @@ class CustomCmds(commands.Cog):
     @commands.hybrid_command(aliases=['ab','bot', 'aboutbot'], description="information about the bot") 
     async def botinfo(self, ctx): 
              embed= discord.Embed(title ='<a:gc_ShinyDot:949181495497621514> __Bot Information__ <a:gc_ShinyDot:949181495497621514>',            description=f'{self.bot.description}', color=(cfg.CLR)) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Name', value=f'```{self.bot.user.name}```', inline=False) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Created on', value=f'```{(self.bot.user.created_at.strftime(f"%d/%m/%y"))}```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Library', value=f'```discord.py v2```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Ping', value=f'```{round(self.bot.latency*1000)}ms```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Api version', value=f"```{discord.__version__}```", inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Bot version', value=f'```v5.1```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Developers', value=f'```HELLTRONIX#5209, Poison Tribbiani#0057, Lxghtning~#0388```', inline=False) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Servers', value=f'```{len(self.bot.guilds)}```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Members', value=f'```{len(self.bot.users)}```', inline=True) 
-             embed.add_field(name='<a:black_dot1:949181553295114310> Commands', value=f'```{len(self.bot.commands)}```', inline=True) 
-  
+             embed.add_field(name='<:fancyarr:1064443589825933343> Name', value=f'```{self.bot.user.name}```', inline=False) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Created on', value=f'```{(self.bot.user.created_at.strftime(f"%d/%m/%y"))}```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Library', value=f'```discord.py v2```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Ping', value=f'```{round(self.bot.latency*1000)}ms```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Api version', value=f"```{discord.__version__}```", inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Bot version', value=f'```v5.1```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Developers', value=f'```HELLTRONIX#5209, Poison Tribbiani#0057, Lxghtning~#0388```', inline=False) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Servers', value=f'```{len(self.bot.guilds)}```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Members', value=f'```{len(self.bot.users)}```', inline=True) 
+             embed.add_field(name='<:fancyarr:1064443589825933343> Commands', value=f'```{len(self.bot.commands)}```', inline=True) 
+             embed.color(cfg.CLR)
              embed.set_footer(text = f'{self.bot.user.name} | {self.bot.user.id}', icon_url=self.bot.user.avatar.url) 
              embed.timestamp = discord.utils.utcnow() 
              await ctx.send(embed = embed)    
@@ -272,7 +272,7 @@ class CustomCmds(commands.Cog):
         embed4.description = desc4
         embed5 = discord.Embed(color=cfg.CLR)
         embed5.set_image(url="https://cdn.discordapp.com/attachments/1150321238997205002/1150333919112216606/rules.png")
-        embeds = [embed1, embed2, embed3, embed4]
+        embeds = [embed1, embed2, embed3, embed4, embed5]
         await ctx.send(embeds=embeds)
         await ctx.message.delete()
 
@@ -286,14 +286,14 @@ desc1 = """
 
                <:_:1034833534164140104>**Moderation System**<:_:1034833534164140104>
 
-<:_:1034833385543172136> **__BASIC INFORMATION__**
+<:reddrop:1064512185721704458> **__BASIC INFORMATION__**
 > Moderation Team will impose strikes according to the below mentioned __Strike Policy__ taking into account your history & graveness of your violation of the rules. All actions are taken after thorough investigation & collection of evidence.
-<:_:1034833385543172136> **__STRIKE POLICY__**
+<:reddrop:1064512185721704458> **__STRIKE POLICY__**
 > First Strike: Warning
 > Second Strike: Mute (24 Hours)
 > Third Strike: Ban (3 Days)
 > Fourth Strike: Permanent Ban
-<:_:1034833385543172136> **__NOTE__**
+<:reddrop:1064512185721704458> **__NOTE__**
 > Number of strikes given depends on the situation. We can give four strikes together also so be aware & follow all the rules.
 
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧"""
@@ -302,20 +302,20 @@ desc2 = """
 ㅤㅤ     **RULES OF GRAPHICS CODE**
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧
 
-               <:_:1034833534164140104>**General Instructions**<:_:1034833534164140104>
+               <:red_star:1150362128797155398>General Instructions**<:red_star:1150362128797155398>
 
-<:_:1034833385543172136> **__NO DEFAMATORY LANGUAGE__**
+<:reddrop:1064512185721704458> **__NO DEFAMATORY LANGUAGE__**
 > Using defamatory words or cussing anyone in any situation with the intention to harm anyone mentally is strictly prohibited. Using such language casually & to improvise humour is allowed upto a certain mark.
-<:_:1034833385543172136> **__NO NSFW CONTENT__**
+<:reddrop:1064512185721704458> **__NO NSFW CONTENT__**
 > Sharing sexually implicit content or non-consensual intimate images/links is not allowed.
-<:_:1034833385543172136> **__NO SPAMMING/EARRAPING__**
+<:reddrop:1064512185721704458> **__NO SPAMMING/EARRAPING__**
 > Spamming of every kind whether it is text-based or voice chat based including earraping, playing discomposed music & purposeful intrusion in discussions of others during day-to-day activity or events is strictly prohibited.
-<:_:1034833385543172136> **__TREAT EVERYONE EQUALLY __**
+<:reddrop:1064512185721704458> **__TREAT EVERYONE EQUALLY __**
 > Treat everyone equally with respect & appreciate sentiments of everyone irrespective of caste, creed, gender, religion & other beliefs.
-<:_:1034833385543172136> **__NO UNETHICAL ADVERTISING__**
+<:reddrop:1064512185721704458> **__NO UNETHICAL ADVERTISING__**
 > Promoting unlawful/violent content or product is not allowed.
 > DM Promotion through individuals or bots targeting people of our server is not allowed.
-<:_:1034833385543172136> **__NO ILLEGAL ACTIVITIES__**
+<:reddrop:1064512185721704458> **__NO ILLEGAL ACTIVITIES__**
 > Practicing disobedient behaviour or sharing private information about others without their consent & posting malicious links to fraud/scam websites, supporting hacking or DDOS attacks is strictly prohibited.
 > Trading illegal things of any sort for personal interest is also prohibited.
 
@@ -325,17 +325,17 @@ desc3 = """
 ㅤㅤ     **RULES OF GRAPHICS CODE**
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧
 
-               <:_:1034833534164140104>**Ticket Instructions**<:_:1034833534164140104>
+               <:red_star:1150362128797155398>**Ticket Instructions**<:red_star:1150362128797155398>
 
-<:_:1034833385543172136> **__ORDER DISCUSSIONS__**
+<:reddrop:1064512185721704458> **__ORDER DISCUSSIONS__**
 > No discussions related to the order to be done in DMs or in any other channel. All order related talks need to be done in the ticket itself including placement of orders, asking for updates, payment process & claiming the design as everything needs to be logged for further use.
-<:_:1034833385543172136> **__CODE OF CONDUCT__**
+<:reddrop:1064512185721704458> **__CODE OF CONDUCT__**
 > Fill out the forms correctly while placing the orders.
 > Be a little elaborate to everything you mention for giving the designer a better idea to take care of your needs.
 > Insulting or disrespecting any staff/designer for your personal interest or with the intention to defame anyone is not allowed. 
-<:_:1034833385543172136> **__TICKET CREATION__**
+<:reddrop:1064512185721704458> **__TICKET CREATION__**
 > Create tickets only if you meet the order requirements & are willing to buy something. Don't open tickets with the intention of wasting time & efforts of others.
-<:_:1034833385543172136> **__DESIGN PRICES __**
+<:reddrop:1064512185721704458> **__DESIGN PRICES __**
 > Negotiation in prices is not allowed. Minimum price of every design is fixed & everyone need to follow up that unless any event/offer is in progress. 
 
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧"""
@@ -344,13 +344,13 @@ desc4 = """
 ㅤㅤ     **RULES OF GRAPHICS CODE 2.0**
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧
 
-               <:_:1034833534164140104>**Things to be Noted**<:_:1034833534164140104>
+               <:red_star:1150362128797155398>**Things to be Noted**<:red_star:1150362128797155398>
 
-<:_:1034833385543172136> **__ALTS/MULTIPLE ACCOUNTS__**
+<:reddrop:1064512185721704458> **__ALTS/MULTIPLE ACCOUNTS__**
 > Using multiple accounts for trolling/disturbing others, evading bans, mutes & other restrictions or to increase chances of winning any sort of giveaway/event is strictly prohibited.
-<:_:1034833385543172136> **__MESSAGING STAFFS__**
+<:reddrop:1064512185721704458> **__MESSAGING STAFFS__**
 > Messaging staff or management on personal window for any sort of query related to this server is prohibited. Instead use ModMail to connect with higher authorities of the server.
-<:_:1034833385543172136> **__LOOPHOLES__**
+<:reddrop:1064512185721704458> **__LOOPHOLES__**
 > In case you find something unusual happening or any sort of error in any part of the server including the Official Bot, make sure to report it to the moderators or higher authorities promptly. Exploiting loopholes or encouraging such kind of action is strictly prohibited.
 
 ⪦━━━━━━━━━━━━━━━━━━━━━━━⪧"""
