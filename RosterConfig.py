09473e0bbb19fe472e89956c44b32d7e6aca62c8
a3r0 <nitusingh14372@gmail.com>
2023-09-14 19:27:58 +0530

    sheesh

--- RosterConfig.py
+++ RosterConfig.py
@@ -1,6 +1,6 @@
 import config as cfg
 emoji = "<:gc:1150762439789527100>"
 channel = 1151875624412139563
-msgid = 1025029051057971293    # Put msg if the roster embed. Use `send_roster` if you dont hv the management roster
+msgid = 1151879207647981578    # Put msg if the roster embed. Use `send_roster` if you dont hv the management roster
 
 CLR = cfg.CLR
