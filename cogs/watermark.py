362ecc845675e51b312da5779c759f63942035c2
a3r0 <nitusingh14372@gmail.com>
2023-10-28 14:34:41 +0530

    okl

--- cogs/watermark.py
+++ cogs/watermark.py
@@ -25,6 +25,47 @@ class Watermark(commands.Cog):
             await ctx.send("Please upload an image or provide an image link.")
             return
 
+        # Define a function to process the watermark and image
+        async def process_image(image):
+            try:
+                # Open the image and the watermark
+                watermark = Image.open("assets/wm.png")
+
+                # Get image dimensions and calculate the watermark size based on the shorter side
+                input_width, input_height = image.size
+                min_dimension = min(input_width, input_height)
+
+                # Calculate the scaling ratio for the watermark
+                scale_factor = min_dimension / 10  # Define a scale factor (you can adjust this value)
+
+                # Resize the watermark based on the scale factor
+                new_width = int(watermark.width * scale_factor)
+                new_height = int(watermark.height * scale_factor)
+                watermark = watermark.resize((new_width, new_height))
+
+                # Calculate the position to paste the watermark at the center
+                paste_x = (input_width - new_width) // 2
+                paste_y = (input_height - new_height) // 2
+
+                # Create a transparent layer to paste the watermark onto
+                transparent = Image.new('RGBA', (input_width, input_height), (0, 0, 0, 0))
+                transparent.paste(watermark, (paste_x, paste_y), watermark)
+
+                # Apply watermark on the image
+                watermarked_image = Image.alpha_composite(image.convert("RGBA"), transparent)
+                watermarked_image = watermarked_image.convert("RGB")
+
+                # Convert the watermarked image into bytes
+                img_byte_array = BytesIO()
+                watermarked_image.save(img_byte_array, format='PNG')
+                img_byte_array.seek(0)
+
+                # Send the watermarked image in the chat
+                await ctx.send(file=discord.File(img_byte_array, filename="watermarked_image.png"))
+
+            except Exception as e:
+                await ctx.send(f"An error occurred: {e}")
+
         # If an image link is provided, fetch the image using the link
         if image_link:
             response = requests.get(image_link)
@@ -32,46 +73,14 @@ class Watermark(commands.Cog):
                 await ctx.send("Failed to fetch the image from the provided link.")
                 return
             image_data = BytesIO(response.content)
+            image = Image.open(image_data)
+            await process_image(image)
         else:
             # If an image is uploaded, retrieve the attachment
             attachment = ctx.message.attachments[0]
             image_data = BytesIO(await attachment.read())
-
-        try:
-            # Open the image and the watermark
             image = Image.open(image_data)
-            watermark = Image.open("assets/wm.png")
-
-            # Get image dimensions and calculate the watermark size based on the shorter side
-            input_width, input_height = image.size
-            min_dimension = min(input_width, input_height)
-
-            # Calculate the scaling ratio for the watermark
-            scale_factor = min_dimension / 10  # Define a scale factor (you can adjust this value)
-
-            # Resize the watermark based on the scale factor
-            new_width = int(watermark.width * scale_factor)
-            new_height = int(watermark.height * scale_factor)
-            watermark = watermark.resize((new_width, new_height))
-
-            # Calculate the position to paste the watermark at the center
-            paste_x = (input_width - new_width) // 2
-            paste_y = (input_height - new_height) // 2
-
-            # Create a transparent layer to paste the watermark onto
-            transparent = Image.new('RGBA', (input_width, input_height), (0, 0, 0, 0))
-            transparent.paste(watermark, (paste_x, paste_y), watermark)
-
-            # Apply watermark on the image
-            watermarked_image = Image.alpha_composite(image.convert("RGBA"), transparent)
-            watermarked_image = watermarked_image.convert("RGB")
-
-            # Save the watermarked image and send it in the chat
-            watermarked_image.save("watermarked_image.png")
-            await ctx.send(file=discord.File("watermarked_image.png"))
-
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
+            await process_image(image)
 
 async def setup(bot):
     await bot.add_cog(Watermark(bot))
