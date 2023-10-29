e08eb18e147e6a3d263798a4af743f88151fb43c
a3r0 <nitusingh14372@gmail.com>
2023-10-29 18:01:10 +0530

    buddha b c

--- cogs/watermark.py
+++ cogs/watermark.py
@@ -4,10 +4,6 @@ from PIL import Image
 import requests
 from io import BytesIO
 import concurrent.futures
-import time
-import config as cfg
-
-CLR = cfg.CLR  # Set your color code
 
 class Watermark(commands.Cog):
     def __init__(self, bot):
@@ -25,35 +21,23 @@ class Watermark(commands.Cog):
         The command checks for uploaded images or provided links. It fetches the image and applies a watermark.
         """
 
-        # Function to apply watermark and collect diagnostic details
         def apply_watermark(image):
             watermark = Image.open("assets/wm.png")
-            watermark_reso = watermark.size
-            image_reso = image.size
+            input_width, input_height = image.size
+            watermark = watermark.resize((input_width // 3, input_height // 3))
 
-            start_time = time.time()
-            # Apply the watermark
-            try:
-                input_width, input_height = image.size
-                watermark = watermark.resize((input_width // 1.5, input_height // 1.5))
+            watermark_width, watermark_height = watermark.size
+            paste_x = (input_width - watermark_width) // 2
+            paste_y = (input_height - watermark_height) // 2
 
-                watermark_width, watermark_height = watermark.size
-                paste_x = (input_width - watermark_width) // 2
-                paste_y = (input_height - watermark_height) // 2
+            transparent = Image.new('RGBA', (input_width, input_height), (0, 0, 0, 0))
+            transparent.paste(watermark, (paste_x, paste_y), watermark)
 
-                transparent = Image.new('RGBA', (input_width, input_height), (0, 0, 0, 0))
-                transparent.paste(watermark, (paste_x, paste_y), watermark)
+            watermarked_image = Image.alpha_composite(image.convert("RGBA"), transparent)
+            watermarked_image = watermarked_image.convert("RGB")
 
-                watermarked_image = Image.alpha_composite(image.convert("RGBA"), transparent)
-                watermarked_image = watermarked_image.convert("RGB")
+            return watermarked_image
 
-                watermark_time = time.time() - start_time
-
-                return watermarked_image, watermark_reso, image_reso, watermark_time
-            except Exception as e:
-                return f"An error occurred: {e}", watermark_reso, image_reso, 0
-
-        # Fetch the image and process with threading
         if image_link:
             response = requests.get(image_link, stream=True)
             if response.status_code != 200:
@@ -69,26 +53,15 @@ class Watermark(commands.Cog):
             image_data = BytesIO(await attachment.read())
             image = Image.open(image_data)
 
-        diagnostic_embed = discord.Embed(title="Watermark Diagnostics", color=CLR)
-        
         try:
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                result = await self.bot.loop.run_in_executor(executor, apply_watermark, image)
+                watermarked_image = await self.bot.loop.run_in_executor(executor, apply_watermark, image)
             
-            if isinstance(result[0], Image.Image):
-                watermarked_image, watermark_reso, image_reso, watermark_time = result
+            img_byte_array = BytesIO()
+            watermarked_image.save(img_byte_array, format='PNG')
+            img_byte_array.seek(0)
 
-                diagnostic_embed.add_field(name="Watermark Resolution", value=str(watermark_reso))
-                diagnostic_embed.add_field(name="Image Resolution", value=str(image_reso))
-                diagnostic_embed.add_field(name="Watermark Application Time", value=str(f"{watermark_time:.3f} seconds"))
-
-                img_byte_array = BytesIO()
-                watermarked_image.save(img_byte_array, format='PNG')
-                img_byte_array.seek(0)
-
-                await ctx.send(embed=diagnostic_embed, file=discord.File(img_byte_array, filename="watermarked_image.png"))
-            else:
-                await ctx.send(result[0])
+            await ctx.send(file=discord.File(img_byte_array, filename="watermarked_image.png"))
 
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
